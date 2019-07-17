43e318d1598ef4d651d1d476c2e7a323802205ca
fengxing <f296016140@gmail.com>
2019-07-17 14:33:38 +0800

    RQ: 更新

--- android/src/main/java/com/example/hello/HelloPlugin.java
+++ android/src/main/java/com/example/hello/HelloPlugin.java
@@ -6,20 +6,26 @@ import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
 
-/** HelloPlugin */
+/**
+ * HelloPlugin
+ */
 public class HelloPlugin implements MethodCallHandler {
-  /** Plugin registration. */
-  public static void registerWith(Registrar registrar) {
-    final MethodChannel channel = new MethodChannel(registrar.messenger(), "hello");
-    channel.setMethodCallHandler(new HelloPlugin());
-  }
-
-  @Override
-  public void onMethodCall(MethodCall call, Result result) {
-    if (call.method.equals("getPlatformVersion")) {
-      result.success("Android fengxing " + android.os.Build.VERSION.RELEASE);
-    } else {
-      result.notImplemented();
+    /**
+     * Plugin registration.
+     */
+    public static void registerWith(Registrar registrar) {
+        final MethodChannel channel = new MethodChannel(registrar.messenger(), "hello");
+        channel.setMethodCallHandler(new HelloPlugin());
     }
-  }
+
+    @Override
+    public void onMethodCall(MethodCall call, Result result) {
+        if (call.method.equals("getPlatformVersion")) {
+            result.success("Android fengxing " + android.os.Build.VERSION.RELEASE);
+        } else if (call.method.equals("showToast")) {
+            result.success("showToast = " + call.arguments);
+        } else {
+            result.notImplemented();
+        }
+    }
 }
--- lib/hello.dart
+++ lib/hello.dart
@@ -3,11 +3,15 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 
 class Hello {
-  static const MethodChannel _channel =
-      const MethodChannel('hello');
+  static const MethodChannel _channel = const MethodChannel('hello');
 
   static Future<String> get platformVersion async {
     final String version = await _channel.invokeMethod('getPlatformVersion');
+    return version;
+  }
+
+  static Future<String> toast(String msg) async {
+    final String version = await _channel.invokeMethod('showToast', msg);
     return version;
   }
 }
